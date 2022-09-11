833c6de2946b99f86d586f75db6b3e71173622e0
Makena Hildah <makenabenedict@gmail.com>
2022-09-11 07:25:57 -0700

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,7 @@ int main(void)
         long li;
         long lli;
         float f;
-
+ 
         printf("size of char: %ld byte(s)\n", sizeof(c));
         printf("size of int: %ld byte(s)\n", sizeof(i));
-        printf("size of long int: %ld byte(s)\n", sizeof(li));
-        printf("size of long long int: %ld byte(s)\n", sizeof(lli));
-        printf("size of float %ld byte(s)\n", sizeof(f));
-}
+        printf("size of long int: %ld byte(s)\n". sizeof(li));
