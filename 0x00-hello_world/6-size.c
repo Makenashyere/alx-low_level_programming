df026d62315e97ea96b32d382a10a8bab0faaab4
Makena Hildah <makenabenedict@gmail.com>
2022-09-12 10:22:41 -0700

    4-puts.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main(void)
+
 {
-        char c;
-        int i;
-        long li;
-        long lli;
-        float f;
- 
-        printf("size of char: %ld byte(s)\n", sizeof(c));
-        printf("size of int: %ld byte(s)\n", sizeof(i));
-        printf("size of long int: %ld byte(s)\n". sizeof(li));
+         char c;
+         int i;
+         long i;
+         long lli;
+         float f;
+
+         printf("size of char: %ld byte(s)\n", sizeof(c));
+         printf("size of int: %ld byte(s)\n", sizeof(i));
+         printf("size of long int: %ld byte(s)", sizeof(li);
+         printf("size of long long int: %ld byte(s)", sizeof(lli));
