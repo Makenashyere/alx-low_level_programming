ea434d97c0add41e6539276cbfaa28e4ac197a90
Makena Hildah <makenabenedict@gmail.com>
2022-09-12 13:49:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main(void)
+/**
+* main - prints line followed by new line
+* description - prints line
+* return - (0)
+*/
+
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("sizeof char: %d byte(s)\n", sizeof(char));
+printf("size of int: %d byte(s)\n", sizeof(int));
+printf("size of long int: %d byte(s)\n", sizeof(long int));
+printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of float: %d byte(s)\n", sizeof(float));
+
+return (0);
 }
