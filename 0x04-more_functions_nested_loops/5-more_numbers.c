ba93c002c1683f7ef99d35e5d00e9d8d42c5bc1a
Makena Hildah <makenabenedict@gmail.com>
2022-09-16 05:06:30 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*more_numbers - prints number to 14
+*
+*return: nothing
+*/
+
+void more_numbers(void)
+{
+     int n1, n2;
+     
+     for (n1 = 0; n1 < 10; n1++)
+     {
+            for(n2 = 0; n2 <= 14; n2++)
+            {
+                     if (n2 > 9)
+                     {
+                              putchar((n2/10)+ '0');
+            }
+            putchar(10);
+      }
+}
