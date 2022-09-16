8037e264a06ab9a02925b4e84245e2becb0624c1
Makena Hildah <makenabenedict@gmail.com>
2022-09-16 02:06:37 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_isupper - checks if uppercase
+*@c: character to be checked
+*Return: 1 if uppercase
+*/
+
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+    {
+          Return (1);
+    }
+return(0);
+}
