048a611bb63e657e673162b5756be7c7c6b3d044
Makena Hildah <makenabenedict@gmail.com>
2022-09-20 04:30:18 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 include "main.h"
 
-/**
-* reset_to_98 - updates the value of variable
-* @n: pointer to the variable
-*/
 
+/**
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
 void reset_to_98(int *n)
 {
-        *n = 98;
+	*n = 98;
 }
