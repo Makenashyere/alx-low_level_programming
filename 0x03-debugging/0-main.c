f1a1ef2518f667adc702395df98cba27d6f188dd
Makena Hildah <makenabenedict@gmail.com>
2022-09-22 13:15:45 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if int is pos or neg
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+	return (0);
+}
