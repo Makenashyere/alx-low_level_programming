f14d175009e20864780aacd65ca1cf19ec4d36ab
Makena Hildah <makenabenedict@gmail.com>
2022-09-22 15:35:23 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** print_rev - prints a string in reverse, followed by a new line
+/**
+ *print_rev - prints a string in reverse, followed by a new line
  * @s: string 
  */
 void print_rev(char *s)
