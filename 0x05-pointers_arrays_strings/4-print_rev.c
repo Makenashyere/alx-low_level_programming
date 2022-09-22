a566b328ceca39a38bead15a67d62165cbeae4f5
Makena Hildah <makenabenedict@gmail.com>
2022-09-22 15:31:20 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** print_rev - prints a string in reverse, followed by a new line
+ * @s: string 
+ */
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	int = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n;);
+}
