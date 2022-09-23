4986901861eb353fb7ebe9159a21eff002f0ca8d
Makena Hildah <makenabenedict@gmail.com>
2022-09-23 10:51:40 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
