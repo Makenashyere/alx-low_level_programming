05cca77b5a04350e58bca96cb36292acf1a191c1
Makena Hildah <makenabenedict@gmail.com>
2022-09-23 09:42:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - prints string
- * Return: 0
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
-}
