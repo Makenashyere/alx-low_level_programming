780bfa69a4933af963140707788dcb8c0ae6c623
Makena Hildah <makenabenedict@gmail.com>
2022-09-23 09:13:52 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-/*
+
+/**
 * main - prints string to stdout
 * 
 * 
 * Description - prints string 
-* return 0 - success
+* Return 0 - success
 */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
