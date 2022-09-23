16f42a74d65393bfac8a3b1748bcc2a62a4d3ee7
Makena Hildah <makenabenedict@gmail.com>
2022-09-23 12:37:56 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the int
+ * Return: 1 
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('_');
+	}
+
+	return (-1);
+}
