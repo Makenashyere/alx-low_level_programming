7bf36f92f9fc1319c28a29582aa8d43178e0eacc
Makena Hildah <makenabenedict@gmail.com>
2022-09-23 08:42:50 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,57 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_buffer - C function that the content
- * inputted number of bytes 
- * prints 10 bytes per line.
- * starting with `0`.
- * @b: number of bytes
- * @size: size of the byte
+ * print_line - prints a bytes of a buffer
+ * @c: buffer @s: bytes of buffer
+ * @l: line of buffer 
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf(" ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 +k] < 127)
+			putchar(c[l * 10 +k]);
+		else
+			putchar('-');
+	}
+}
+
+/**
+ * print_buffer - print a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j;
-
-	if (size < 0)
+	int i;
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf('\n');
-		return;
-	}
-
-	while (i < size)
-	{
-		if (i % 10 == 0)
-			printf("%08x: ", i);
-		for (j = i; j < i + 9; j+=2)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
-			if ((j< size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
-			else
-			{
-				while (++j <= i + 10)
-					printf(" ");
-			}
+			print_line(b, 9, i);
 		}
-		for (j = i; j < i + 9 && j < size; j++)
+		else
 		{
-			if  (b[j] >= 32 && b[j] <== 126)
-				printf("%c", b[j]);
-			else
-				printf(".");
+			print_line(b, size % 10 - 1, i);
 		}
-		printf('\n');
-		i += 10;
+		putchar('\n');
 	}
+	if (size == 0)
+		putchar('\n');
 }
