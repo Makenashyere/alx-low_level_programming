8dad7d7c7f4bbf97b8fdd083fba6b5d8f44b087d
Makena Hildah <makenabenedict@gmail.com>
2022-09-23 09:04:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /*
 * main - prints out string to stdout
 *
-*
 * Description - prints out string
-* return - 0 if success
+* Return - 0 if success
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
