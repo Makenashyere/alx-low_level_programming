7d76f1fef6034df359d92594d9fa27341bab2764
Makena Hildah <makenabenedict@gmail.com>
2022-11-03 06:18:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,5 +9,6 @@
  */
 int main(void)
 {
-	write(2,stderr "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(2,stderr "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
 	return (1);
