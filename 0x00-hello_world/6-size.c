427210b94674eb9bcef0aad561b2bc3420ac8e7b
Makena Hildah <makenabenedict@gmail.com>
2022-11-03 06:04:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,7 @@
 
 /**
 * main - prints line followed by new line
-* description - prints line
-* Return: - (0)
+* Return: 0
 */
 int main(void)
 {
@@ -11,7 +10,6 @@ int main(void)
 	printf("size of int: %d byte(s)\n", sizeof(int));
 	printf("size of long int: %d byte(s)\n", sizeof(long int));
 	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of float: %d byte(s)\n", sizeof(float));
-
-	return (0);
+	printf("size of float: %d byte(s)\n", sizeof(float))
+        return (0);
 }
