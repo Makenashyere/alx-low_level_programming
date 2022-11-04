001b5f21d90e37a22e030c38139e3b83aef24aee
Makena Hildah <makenabenedict@gmail.com>
2022-11-04 06:35:29 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1st 50 Fibonacci starting with 1 and 2 then new line
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, j, k, next;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; i++)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		} else
+		{
+			print("%ld\n". j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
