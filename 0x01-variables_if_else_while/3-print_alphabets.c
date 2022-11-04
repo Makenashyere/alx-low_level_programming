d0139cdd82ebc86ecf8e387962a1c1db0e49559d
Makena Hildah <makenabenedict@gmail.com>
2022-11-03 23:42:21 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	int i:
+	int i;
 
-	for (i='a'; i<='z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
-	for (i='A'; i<='Z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
