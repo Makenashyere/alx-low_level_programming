6c59acfc09b96dd229908a182c2594322a38f929
Makena Hildah <makenabenedict@gmail.com>
2022-11-04 05:47:12 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,8 +6,8 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-
-		if (n >= 0 && n <+ 15)
+	
+	if (n >= 0 && n <= 15)
 		{
 			for (i = 0; i <= n; i++)
 			{
@@ -15,7 +15,7 @@ void print_times_table(int n)
 				{
 					k = j * i;
 					if (j == 0)
-					{ 
+					{
 						_putchar(k + '0');
 					} else if (k < 10 && j != 0)
 					{
@@ -31,7 +31,7 @@ void print_times_table(int n)
 						_putchar(' ');
 						_putchar((k / 10) + '0');
 						_putchar((k % 10) + '0');
-					} else if (k >=100)
+					} else if (k >= 100)
 					{
 						_putchar(',');
 						_putchar(' ');
