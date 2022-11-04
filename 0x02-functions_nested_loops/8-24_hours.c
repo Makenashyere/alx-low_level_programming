9c60cd3e77d381d65e15d380e61193ce4f776f52
Makena Hildah <makenabenedict@gmail.com>
2022-11-04 01:03:03 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,7 +17,7 @@ void jack_bauer(void)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-			-putchar(':');
+			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
