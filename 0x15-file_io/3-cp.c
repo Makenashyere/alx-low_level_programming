6c2b14eb177379130150cfb1cc59483bd68f0919
Makena Hildah <makenabenedict@gmail.com>
2022-12-30 05:16:24 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ char *create_buffer(char *file)
 
 	if (buffer == NULL)
 	{
-		dprint(STDERR_FILENO,
+		dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", file);
 		exit(99);
 	}
@@ -39,7 +39,7 @@ void close_file(int fd)
 		
 	if (c == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
