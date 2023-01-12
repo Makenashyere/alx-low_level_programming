e91bfa2e9e1efc60cf6fd93b26778ad3c2992474
makena Hildah <makenabenedict@gmail.com>
2023-01-11 22:55:11 -0800

    0-hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,29 @@
-def hash_table_create(size): 
-    # Create an empty hash table 
-    hash_table = [None] * size 
-    # return the hash table 
-    return hash_table
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [None] * self.size
+
+    def hash_function(self, key):
+        return key % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value)]
+        else:
+            self.table[index].append((key, value))
+
+    def search(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is None:
+            return None
+        else:
+            for k, v in self.table[index]:
+                if k == key:
+                    return v
+            return None
+
+def hash_table_create(size):
+    if size <= 0:
+        return None
+    return HashTable(size)
